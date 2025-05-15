f4e029fce99d2036aed201eac553abb561be4ea4
egorberd <egorberd@gmail.com>
2025-05-15 22:29:45 +0300

    Initial commit

---
+++ Storage/Database/Module_DB.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.Data.Sqlite;
+using System.Globalization;
+
+namespace Storage.Database
+{
+    public class Module_DB
+    {
+        // Инициализируем переменную которая будет хранить имя файла БД
+        private static string dbFileName = "storage.db";
+
+        // Объявляем строку для подключение к БД
+        private static string connectionString = $"Data Source={dbFileName};";
+
+        // Функция основного взаимодействия с БД
+        public static void DataBaseInteraction()
+        {
+            // Настройка привязки библиотеки для последующего подключения к БД
+            SQLitePCL.Batteries.Init();
+
+            // Проверка на наличие файла базы данных в директории запуска
+            if (!File.Exists(dbFileName))
+            {
+                Console.WriteLine($"Файл базы данных '{dbFileName}' не найден.");
+            }
+
+            // Инициализируем два списка объектов класса Box и Pallet
+            List<Box> boxes = LoadBoxesFromDatabase(); // Вызываем метод для выгрузки данных коробок из БД
+            List<Pallet> pallets = LoadPalletsFromDatabase(); // Вызываем метод для выгрузки данных паллет из БД
+
+            // Проверка на наличие данных в списках паллет и коробок
+            if (pallets.Any() && boxes.Any())
+            {
+                foreach (var box in boxes)
+                {
+                    pallets[0].AddBox(box); // Добавляем все коробки к первой паллете
+                }
+            }
+
+            // Выводим информацию по коробкам в консоль
+            Console.WriteLine("Коробки: ");
+            foreach (var box in boxes)
+            {
+                Console.WriteLine(box);
+            }
+
+            // Выводим информацию по паллетам в консоль
+            Console.WriteLine("\nПаллеты: ");
+            foreach (var pallet in pallets)
+            {
+                Console.WriteLine(pallet);
+
+                // Выводим коробки стоящие на определенной паллете:
+                if (pallet.boxes.Any())
+                {
+                    Console.WriteLine("  Коробки на этой паллете: ");
+                    foreach (var boxOnPallet in pallet.boxes)
+                    {
+                        Console.WriteLine($"    - {boxOnPallet}");
+                    }
+                }
+            }
+        }
+
+        // Функция для загрузки данных о коробоках из БД
+        private static List<Box> LoadBoxesFromDatabase()
+        {
+            List<Box> boxes = new List<Box>();
+
+            // Устанавливаем соединение с БД SqLite
+            using (SqliteConnection connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+                // Пишем SQL запрос для получения всех данных таблицы Boxes
+                string query = "SELECT ID, Width, Height, Depth, Weight, ProductionDate, ExpirationDate FROM Boxes";
+                using (SqliteCommand command = new SqliteCommand(query, connection))
+                {
+                    using (SqliteDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            // Создаем объект класса Box и присваиваем ему данные из БД
+                            Box box = new Box(
+                                Width: Convert.ToDouble(reader["Width"]),
+                                Height: Convert.ToDouble(reader["Height"]),
+                                Depth: Convert.ToDouble(reader["Depth"]),
+                                Weight: Convert.ToDouble(reader["Weight"]),
+
+                                // т.к. SQLite хранит дату как текст мы используем DateTime.ParseExact
+                                ExpirationDate: DateOnly.FromDateTime(DateTime.ParseExact(reader["ExpirationDate"].ToString(), "MM/d/yyyy", CultureInfo.InvariantCulture)),
+                                ProductionDate: reader["ProductionDate"] == DBNull.Value || string.IsNullOrEmpty(reader["ProductionDate"].ToString())
+                                    ? (DateOnly?)null
+                                    : DateOnly.FromDateTime(DateTime.ParseExact(reader["ProductionDate"].ToString(), "MM/d/yyyy", CultureInfo.InvariantCulture))
+                            );
+                            box.DbId = Convert.ToInt32(reader["ID"]); // Сохраняем ID из базы данных
+                            boxes.Add(box);
+                        }
+                    }
+                }
+            }
+            return boxes;
+        }
+
+        // Функция для загрузки данных о паллетах из БД SqLite
+        private static List<Pallet> LoadPalletsFromDatabase()
+        {
+            List<Pallet> pallets = new List<Pallet>();
+
+            // Устанавливаем соединение с БД SqLite
+            using (SqliteConnection connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+                // Пишем SQL запрос для получения всех данных таблицы Pallets
+                string query = "SELECT ID, Width, Height, Depth FROM Pallets";
+                using (SqliteCommand command = new SqliteCommand(query, connection))
+                {
+                    using (SqliteDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            // Создаем объект класса Pallet и присваиваем ему данные из БД
+                            Pallet pallet = new Pallet(
+                                Width: Convert.ToDouble(reader["Width"]),
+                                Height: Convert.ToDouble(reader["Height"]),
+                                Depth: Convert.ToDouble(reader["Depth"])
+                            );
+                            pallet.DbId = Convert.ToInt32(reader["ID"]); // Сохраняем ID из базы данных
+                            pallets.Add(pallet);
+                        }
+                    }
+                }
+            }
+            return pallets;
+        }
+    }
+}
---
+++ Storage/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+using System.Data.SQLite;
+using Storage.Database;
+
+// Создаем родительский класс "Склад"
+public abstract class Warehouse
+{
+    // Инициализируем переменные общего набора свойств (ID, ширина, высота, глубина, вес)
+    public Guid id { get; } = Guid.NewGuid();
+    public int DbId { get; set; }
+    public double width { get; set; }
+    public double height { get; set; }
+    public double depth { get; set; }
+    public double weight { get; set; }
+
+    // Инициализируем конструктор родительского класса 
+    protected Warehouse(double Width, double Height, double Depth, double Weight)
+    {
+        width = Width;
+        height = Height;
+        depth = Depth;
+        weight = Weight;
+    }
+
+    // Переопределяем метод ToString() для последующего вывода параметров на экран
+    public override string ToString()
+    {
+        return $"App_ID: {id}, DB_ID: {DbId} Ширина: {width}, Высота: {height}, Глубина: {depth}, Вес: {weight}";
+    }
+}
+
+// Создаем наследуемый класс "Коробка"
+public class Box : Warehouse
+{
+    // Инициализируем переменные срока годности и даты производства
+    public DateOnly expirationDate { get; private set; }
+    public DateOnly? productionDate { get; private set; }
+
+    // нициализируем конструктор наследуемого класса 
+    public Box(double Width, double Height, double Depth, double Weight,
+        DateOnly ExpirationDate, DateOnly? ProductionDate = null) : base(Width, Height, Depth, Weight)
+    {
+
+        productionDate = ProductionDate;
+
+        //Прописываем условие: "Если указана дата производства, то срок годности вычисляется из даты производства плюс 100 дней"
+        expirationDate = productionDate.HasValue ? productionDate.Value.AddDays(100) : expirationDate;
+    }
+
+    // Создаем функцию для рассчёта объема коробки
+    public double CalculateVolume()
+    {
+        return width * height * depth;
+    }
+
+    // Переопределяем метод ToString() для последующего вывода параметров на экран
+    public override string ToString()
+    {
+        string dateInfo = productionDate.HasValue ? $", Дата производства: {productionDate.Value:dd.MM.yyyy}, Срок годности: {expirationDate:dd.MM.yyyy}" : $"Срок годности: {expirationDate:dd.MM.yyyy}";
+
+        return base.ToString() + dateInfo;
+    }
+}
+
+// оздаем наследуемый класс "Паллета"
+public class Pallet : Warehouse
+{
+    // Инициализируем список объектов класса "Коробка" т.к. паллета может содержать в себе коробки
+    public List<Box> boxes = new List<Box>();
+    public const double palletWeight = 30.0;
+
+    // Инициализируем конструктор наследуемого класса меняя базовое значение веса на константу 30.0
+    public Pallet(double Width, double Height, double Depth) : base(Width, Height, Depth, palletWeight)
+    {
+    }
+
+    // Вычисляем срок годности паллеты
+    public DateOnly ExpirationDate
+    {
+        get
+        {
+            if (!boxes.Any())
+            {
+                return DateOnly.MaxValue;
+            }
+            return boxes.Min(box => box.expirationDate);
+        }
+    }
+    // Создаем метод для добавления коробки с проверкой по размерам (по ширине и глубине)
+    public void AddBox(Box box)
+    {
+        foreach (var currentBox in boxes)
+        {
+            if (currentBox.width > width)
+            {
+                throw new ArgumentException(
+                    $"Ширина коробки({currentBox.width}) превышает ширину паллеты({width})");
+            }
+            else if (currentBox.depth > depth)
+            {
+                throw new ArgumentException(
+                    $"Глубина коробки({currentBox.depth}) превышает глубину паллеты({depth})");
+            }
+
+        }
+
+        boxes.Add(box);
+        CalculateWeight();
+    }
+
+    // Создаем метод для удаления коробки
+    /*   public void RemoveBox(Box box)
+       {
+           if (boxes.Remove(box)) CalculateWeight();
+       }*/
+
+    // Создаем функцию для рассчета веса паллеты 
+    public void CalculateWeight()
+    {
+        weight = boxes.Sum(box => box.weight) + palletWeight;
+    }
+
+    // Создаем функцию для рассчета объема паллеты 
+    public double CalculateVolume()
+    {
+        double palletDepth = width * height * depth;
+        return boxes.Sum(box => box.CalculateVolume()) + palletDepth;
+    }
+
+    // Переопределяем метод ToString() для последующего вывода параметров на экран
+    public override string ToString()
+    {
+        return base.ToString() + " Кол-во коробок: " + boxes.Count();
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Module_DB.DataBaseInteraction();
+    }
+}
